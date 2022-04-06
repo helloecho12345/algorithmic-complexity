107869d192c0b8e617367dbccbb3e80fcaabcf57
Esther Cho <helloecho12345@gmail.com>
2022-04-06 13:41:34 +0100

    updated bubble_sort

--- timer.py
+++ timer.py
@@ -1,17 +1,17 @@
 import time 
 
 def bubble_sort(lst):
-  length = len(lst) - 1
-  for i in range(length):
-        for j in range(length - i):
-         if lst[i] > lst[j + 1]:
-              lst[i + 1] = lst[i]
-              lst[i] = lst[i + 1]
-  print(lst)
-            
-  	
+    length = len(lst)
+    for i in range(length):
+        for j in range(length - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j + 1], lst[j] = lst[j], lst[j + 1]
+                print(lst)
+    print(lst)
 
-num_list = [34, 2, 76, 0, -1, 200]
+
+# num_list = [34, 2, 76, 0, -1, 200]
+num_list = [i for i in range(1000)]
 
 def timer(algo, data):
 	before = time.perf_counter()
